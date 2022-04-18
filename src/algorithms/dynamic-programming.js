53ff3460c4dd7ebe39951402f90208a4004f2bc0
Marcel Oliveira <m.rabello23@gmail.com>
2022-04-17 23:45:42 -0300

    add dynamic programming

--- src/algorithms/dynamic-programming.js
+++ src/algorithms/dynamic-programming.js
@@ -1,4 +1,4 @@
-// Constant Time Complexity => O(1)
+// Linear Time Complexity => O(n)
 const bottomUpFibonacci = (n) => {
   if (n === 1 || n === 2) {
     return 1;
@@ -13,5 +13,25 @@ const bottomUpFibonacci = (n) => {
   return arr[n];
 };
 
-console.log("dynamic-programming", bottomUpFibonacci(35));
-console.log("dynamic-programming", bottomUpFibonacci(1000));
+// Linear Time Complexity => O(n)
+const memoizedFibonacci = (n, memo = []) => {
+  if (memo[n]) return memo[n];
+  let result = 0;
+
+  if (n === 1 || n === 2) {
+    result = 1;
+  } else {
+    result = memoizedFibonacci(n - 1, memo) + memoizedFibonacci(n - 2, memo);
+  }
+
+  memo[n] = result;
+  return result;
+};
+
+console.log(`bottom-up Fibonacci - element 20: ${bottomUpFibonacci(20)}`);
+console.log(`bottom-up Fibonacci - element 35: ${bottomUpFibonacci(35)}`);
+console.log(`bottom-up Fibonacci - element 1000: ${bottomUpFibonacci(1000)}`);
+
+console.log(`memoized Fibonacci - element 20: ${memoizedFibonacci(20)}`);
+console.log(`memoized Fibonacci - element 35: ${memoizedFibonacci(35)}`);
+console.log(`memoized Fibonacci - element 1000: ${memoizedFibonacci(1000)}`);
